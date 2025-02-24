63f6ee50f9205fbe2b04c9e892ed9525f2019fd5
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-02-23 21:32:57 -0500

    clean dockerfile and main

--- src/main.py
+++ src/main.py
@@ -2,17 +2,20 @@ from fastapi import FastAPI
 from src.db.database import engine, Base
 from src.api.routes.users import router as users_router
 from fastapi.staticfiles import StaticFiles
+import uvicorn
+import os
 
 app = FastAPI()
 
-# serve react's built files
-app.mount("/", StaticFiles(directory="dist", html=True), name="frontend")
+# # Create tables
+# Base.metadata.create_all(bind=engine)
 
-# Create tables
-Base.metadata.create_all(bind=engine)
+# # Include routers
+# app.include_router(users_router)
 
-# Include routers
-app.include_router(users_router)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))  # cloud run requires PORT env var
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 @app.get("/api/health")
 def health_check():
@@ -21,5 +24,4 @@ def health_check():
 @app.get("/")
 def root():
     return {"message": "Welcome to the Stocks Social Media API!"}
-
 
