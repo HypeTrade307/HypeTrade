ca633e9127dcb44ef2e992322a92d7b11fba1d5f
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-02-23 21:23:09 -0500

    attempting to deploy frontend on gcr

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from src.db.database import engine, Base
 from src.api.routes.users import router as users_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# serve react's built files
+app.mount("/", StaticFiles(directory="dist", html=True), name="frontend")
 
 # Create tables
 Base.metadata.create_all(bind=engine)
@@ -10,6 +14,12 @@ Base.metadata.create_all(bind=engine)
 # Include routers
 app.include_router(users_router)
 
+@app.get("/api/health")
+def health_check():
+    return {"status": "ok"}
+
 @app.get("/")
 def root():
     return {"message": "Welcome to the Stocks Social Media API!"}
+
+
