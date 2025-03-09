0a5d26822ddcc8895491126eac80c35b64a59f71
michaelangell <carril41@purdue.edu>
2025-03-09 03:13:02 -0400

    quick change to make page not found look nicer

--- HypeTrade307/src/pages/Page_Not_found.tsx
+++ HypeTrade307/src/pages/Page_Not_found.tsx
@@ -1,16 +1,21 @@
 import Home_page_button from "./Home_page_button.tsx";
 import Navbar from "../components/NavbarSection/Navbar.tsx";
-
-function Page_Not_found(){
+import AppTheme from '../components/shared-theme/AppTheme';
+import CssBaseline from "@mui/material/CssBaseline";
+function Page_Not_found(props: {disableCustomTheme?: boolean }){
   return (
 
       <>
+          <AppTheme {...props}>
+              <CssBaseline enableColorScheme />
         <Navbar />
+
         <div>
           <h1>PageNotfound</h1>
           <p>pray.</p>
           <Home_page_button />
         </div>
+      </AppTheme>
       </>
   );
 }
