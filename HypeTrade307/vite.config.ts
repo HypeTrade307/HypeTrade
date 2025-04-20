d7c309dfc59643fd39764af861c2cfb21f00641e
Aditya Gandhi <adityagandhi98101@gmail.com>
2025-04-20 17:42:29 -0400

    disabling error checking through vite.config.ts

--- HypeTrade307/vite.config.ts
+++ HypeTrade307/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }, // common for react
+  },
   plugins: [react()],
   server: {
     proxy: {
