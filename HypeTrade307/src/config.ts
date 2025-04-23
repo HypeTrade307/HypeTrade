ff4ad4efcbaef604213693dbe8352c74cd172039
rkcetin <143024611+rkcetin@users.noreply.github.com>
2025-04-22 20:44:36 -0400

    Update config.ts

--- HypeTrade307/src/config.ts
+++ HypeTrade307/src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
-export const API_LOCAL_URL = 'https://hypet-145797464141.us-central1.run.app/api';
+export const API_BASE_URL = 'https://hypet-145797464141.us-central1.run.app/api';
+export const API_LOCAL_URL = 'http://localhost:8080/api';
